f7ee0d09cf0d2eaff829360b35211f838ee7916b
yerakulmanov <73939078+KulmanovYernar@users.noreply.github.com>
2021-12-19 18:30:41 +0600

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/menu.css'
+import './assets/arr.css'
+ import './assets/styles.scss'
+// 
+
+createApp(App).mount('#app')
